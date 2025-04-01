32653c0ae23a21750c2127dd1497e0a3630fdbfe
krohan10 <kattullarohan@outlook.com>
2025-04-01 14:24:11 +0530

    intial commit

---
+++ Constructor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Constructor
+{
+    public class Customer
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public string GetUserName()
+        {
+            return FirstName + "_" + LastName;
+        }
+        public Customer()
+        {
+            Console.WriteLine("Before: " + Id);
+            Id++;
+        }
+    }
+}
---
+++ ConstructorDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ContructorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            Console.WriteLine("After:" + c1.Id);
+
+            Customer c2 = new Customer();
+            Console.WriteLine("After:" + c1.Id);
+
+            Customer c3 = new Customer();
+            Console.WriteLine("After:" + c1.Id);
+
+            Customer c4 = new Customer();
+            Console.WriteLine(c4.Id);
+        }
+
+        public class Customer
+        {
+            public int Id;
+            public string FirstName;
+            public string LastName;
+            public string GetUserName()
+            {
+                return FirstName + "_" + LastName;
+            }
+
+      
+            public Customer()
+            {
+                Console.WriteLine("Before:" + Id);
+                Id++;
+
+            }
+        }
+    }
+}
